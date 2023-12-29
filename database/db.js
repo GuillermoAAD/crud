e1e630536235668728016b87a3ecee79df927908
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2023-12-29 09:54:10 -0600

    [U] db.js

--- database/db.js
+++ database/db.js
@@ -4,11 +4,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({path: '../.env'})
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_CONNECTION,
+  process.env.DB_DATABASE ?? '',
+  process.env.DB_USERNAME ?? '',
+  process.env.DB_PASSWORD ?? '',
+  {
+    host: process.env.DB_HOST ?? 'localhost',
+    dialect: process.env.DB_CONNECTION ?? 'mysql',
+    sync: false, // Desactiva la sincronización automática
 });
 
 module.exports = sequelize;
