4b71b8f7fdec06f3ae10d25b9f5558b602439dae
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2023-12-29 22:00:34 -0600

    [U] migration

--- migrations/20231229160556-create_users_table.js
+++ migrations/20231229160556-create_users_table.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         autoIncrement: true,
       },
-      nombre: {
+      name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
