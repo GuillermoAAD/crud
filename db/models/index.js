9560c695448567c566e75a33a1dae165bffa76d9
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2023-12-29 22:40:18 -0600

    [U] move model, migration, seed into db folder

--- db/models/index.js
+++ db/models/index.js
@@ -7,6 +7,8 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+console.log('config')
+console.log(config)
 const db = {};
 
 let sequelize;
